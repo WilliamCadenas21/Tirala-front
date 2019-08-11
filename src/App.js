f5829bebeca5e0b5930623180d206a21a2aabe9e
williamcadenas21 <williamcadenas21@gmail.com>
2019-08-11 18:38:18 -0500

    update bitacora and agregated the main paths

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
+
+
+import Navigation from './components/Navigation'
+import PostsList from './components/PostsList'
+import CreatePost from './components/CreatePost'
+import CreateUser from './components/CreateUser'
 
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <Router>
+      <Navigation/>
+      <Route path="/" exact component={PostsList}/>
+      <Route path="/edit/:id" component={CreatePost}/>
+      <Route path="/create" component={CreatePost}/>
+      <Route path="/user" component={CreateUser}/>
+      
+    </Router>
   );
 }
 
