6b42f0e3489b57ee11b819760c4116a2cb4e1afa
williamcadenas21 <williamcadenas21@gmail.com>
2019-08-17 15:26:07 -0500

    installing boostrap

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Navigation extends Component {
     render() {
         return (
-            <div>
-                Navigation
-            </div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container">
+                    <Link className="navbar-brand" to="/">
+                        Tirala-app
+                </Link>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item active">
+                                <Link className="nav-link" to="/">Posts</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link className="nav-link" to="/create">Create Notes</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link className="nav-link" to="/user">Create User</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         )
-    }   
+    }
 }
